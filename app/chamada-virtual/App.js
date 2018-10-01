9910ddd97d07cc8e84aef119385f92df053afc1a
daviwesley <daviwesley@ufc.br>
2018-09-30 22:08:15 -0300

    tela cadastro e fazer chamada adicionado

--- app/chamada-virtual/App.js
+++ app/chamada-virtual/App.js
@@ -1,34 +1,90 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Card, Icon } from 'react-native-elements'
+import { StyleSheet, View, TextInput , Text, Button, KeyboardAvoidingView,
+Picker,
+} from 'react-native';
+import { Card, Icon,} from 'react-native-elements'
 import { createStackNavigator } from 'react-navigation';
-import { Col, Row, Grid } from "react-native-easy-grid";
+import { Col, Grid } from "react-native-easy-grid";
+
+//import { Cadastro } from './components/cadastro';
+
+// tela de cadastramento de aluno
+export class Cadastro extends React.Component {
+  render() {
+      return (
+        <KeyboardAvoidingView  behavior="padding" enabled>
+          <Text style={styles.headerText}>Nome do aluno</Text>
+          <TextInput placeholder="Digite o nome do aluno"
+                     style={styles.textInput}
+                     autoCapitalize='words'
+                     returnKeyType='next'
+                     //onSubmitEditing={() => { this.secondTextInput.focus(); }}
+                     blurOnSubmit={false}
+          />
+          <Text style={styles.headerText}>Matricula do aluno</Text>
+          <TextInput placeholder="Digite a matricula do aluno"
+                    style={styles.textInput}
+                    keyboardType='numeric'
+                    maxLength={6}
+          />
+          <Button title="Cadastrar" onPress={() => null }/>
+        </KeyboardAvoidingView>
+      );
+    }
+  }
+
+// Tela da chamada
+  export class ChamadaScreen extends React.Component {
+
+    constructor(){
+      super();
+      this.state={
+        PickerValueHolder : ''
+      }
+    }
+
+    render() {
+        return (
+          <KeyboardAvoidingView  behavior="padding" enabled>
+          <Picker
+            selectedValue={this.state.PickerValueHolder}
+            onValueChange={(itemValue, itemIndex) => this.setState({PickerValueHolder: itemValue})} >
+            <Text>Escolha um aluno</Text>
+            <Picker.Item label="React Native" value="React Native" />
+            <Picker.Item label="Java" value="Java" />
+            <Picker.Item label="Html" value="Html" />
+            <Picker.Item label="Php" value="Php" />
+            <Picker.Item label="C++" value="C++" />
+            <Picker.Item label="JavaScript" value="JavaScript" />
+    
+          </Picker>
+          </KeyboardAvoidingView>
+        );
+      }
+    }
 
 class App extends React.Component {
-  static navigationOptions = ({ navigation }) => {
-  const {state} = navigation;
-  return {
-    title: "Inicio",
-  };
-};
 
 render() {
     return (
       <Grid>
         <Col>
           <Card title="Cadastrar Alunos">
-            <Icon name='group-add'/>
+            <Icon size={35} name='group-add' 
+            onPress={() => this.props.navigation.navigate('CadastroAluno')} />
           </Card>
           <Card title="Fazer chamadas">
-            <Icon name="add"/>
+            <Icon size={35} name="check-circle"
+            />
           </Card>
         </Col>
         <Col>
-          <Card title="Cadastrar Alunos">
-            <Icon name="account"/>
+          <Card title="Fazer Chamada">
+            <Icon size={35} name="check"
+            onPress={() => this.props.navigation.navigate('Chamada')}/>
           </Card>
-          <Card title="Cadastrar Alunos">
-            <Icon name="account"/>
+          <Card title="Sobre Nós" >
+            <Icon size={35} name="live-help"/>
           </Card>
         </Col>
       </Grid>
@@ -39,24 +95,42 @@ render() {
 const navBar = createStackNavigator({
   Home: {
     screen: App,
+    navigationOptions: {
+      title: 'Inicio'
+    }
+  },
+  CadastroAluno: {
+    screen: Cadastro,
+    navigationOptions: {
+      title: 'Cadastrar Aluno'
+    }
+  },
+  Chamada: {
+    screen: ChamadaScreen,
+    navigationOptions: {
+      title: 'Cadastrar Aluno'
+    }
   },
 });
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e5e5e5"
   },
-  containerOptions: {
-    //flexDirection: 'row',
-    justifyContent:'center',
+  textInput: {
+    height: 40, 
+    width: "95%", 
+    borderColor: 'gray', 
+    borderRadius: 2,
+    borderWidth: 1,  
+    marginBottom: 20
   },
-  cardStyle: {
-    alignItems: 'center',
-  },
+  headerText:{
+    fontSize: 15,
+    fontWeight: 'bold',
+  }
 });
-
 export default navBar
