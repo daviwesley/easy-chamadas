31c482ec74b29691b81cf1f552500ed0733d0590
daviwesley <daviwesley@alu.ufc.br>
2018-10-04 23:16:37 -0300

    teste de urls sem token

--- api/api/tests.py
+++ api/api/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase, Client
 from django.utils.timezone import datetime
 from django.db import models
-from model_mommy import mommy
+from django.contrib.auth.models import User
+
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APIClient
 from rest_framework.authtoken.models import Token
+
+from model_mommy import mommy
 
 from api.models import Student
 
@@ -18,29 +21,22 @@ class TestAluno(TestCase):
         self.assertEquals(self.student.__str__(), self.student.name)
 
 class TestURLS(TestCase):
-    fixtures = ['dados_api']
+    #fixtures = ['dados_api']
 
     def setUP(self):
         self.client = Client()
-        self.api_client = APIClient()
+        #self.api_client = APIClient()
 
-    def test_url_api_faults_with_token(self):
-        token = Token.objects.get(user__username='davi')
-        api_client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
-        response = api_client.get('api/faltas')
-        self.assertEquals(response.code, 200)
-        
     def test_url_api_faults_without_token(self):
-        # 403 Forbiden Error
         response = self.client.get('/api/faltas')
         self.assertEquals(response.status_code, 403)
 
     def test_url_api_students(self):
         response = self.client.get('/api/alunos')
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.status_code, 401)
     
     def test_url_api_students_id(self):
         response = self.client.get('/api/alunos/381097')
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.status_code, 401)
     
     
