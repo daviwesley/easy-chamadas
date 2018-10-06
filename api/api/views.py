07f154b7a2cbcc8097c604e148fa4adfa6311d24
daviwesley <daviwesley@alu.ufc.br>
2018-10-06 02:14:42 -0300

    view api disciplina adicionado

--- api/api/views.py
+++ api/api/views.py
@@ -4,8 +4,8 @@ from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 
-from .serializers import StudentSerializer, FaultSerializer, TeacherSerializer
-from .models import Student, Fault, Teacher
+from .serializers import StudentSerializer, FaultSerializer, TeacherSerializer, SubjectSerializer
+from .models import Student, Fault, Teacher, Subject
 
 # Create your views here.
 
@@ -32,4 +32,9 @@ class TeacherViewAPI(generics.ListAPIView):
     permission_classes = (IsAdminUser,)
     serializer_class = TeacherSerializer
     queryset = Teacher.objects.all()
+
+class SubjectViewAPI(generics.ListCreateAPIView):
+    serializer_class = SubjectSerializer
+    queryset = Subject.objects.all()
+    permission_classes = (AllowAny,)
     
