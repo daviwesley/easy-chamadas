5bc3b204abe1a05696b800249596f141b4ba7bf1
daviwesley <daviwesley@alu.ufc.br>
2018-10-06 02:15:12 -0300

    adiciona teste de integração

---
+++ api/api/test/test_integration.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+
+from model_mommy import mommy
+
+from api.models import Student
+
+class TestAPIPost(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.client1 = APIClient()
+        self.token = str(User.objects.create_superuser(username="levi",email=\
+        "levi@levi.com",password="ackerman948").auth_token)
+        self.client.credentials(HTTP_AUTHORIZATION="Token {}".format(self.token))
+    
+    def test_api_create_alunos(self):
+        # Arrange
+        url = '/api/alunos'
+        data = {
+            "name": "Annie Leonheart",
+            "id_subscription": 432634,
+            "course": "CC"
+        }
+        # Act
+        response = self.client.post(url, data, format='json')
+        # Assert
+        self.assertEqual(response.status_code, 201)
+    
+    def test_api_create_subject(self):
+        # Arrange
+        url = '/api/disciplinas'
+        data = {
+            "name": "Inteligência Artificial",
+            "hours": 64,
+            "teacher": [
+                1
+            ]
+        }
+        # Act
+        response = self.client.post(url, data, format='json')
+        # Assert
+        self.assertEqual(response.status_code, 201)
+        
