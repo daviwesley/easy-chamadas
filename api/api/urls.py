a435b8dac5761807ee8423f418b317afc207d237
daviwesley <daviwesley@alu.ufc.br>
2018-10-06 02:14:16 -0300

    url disciplinas adicionado

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views import StudentViewAPI, FaultViewAPI, StudentSearchViewAPI, TeacherViewAPI
+from .views import (StudentViewAPI, FaultViewAPI, StudentSearchViewAPI, TeacherViewAPI,\
+SubjectViewAPI)
 from rest_framework.authtoken import views
 
 urlpatterns = [
@@ -8,5 +9,6 @@ urlpatterns = [
     path('alunos/<int:id>', StudentSearchViewAPI.as_view(), name='procura_aluno'),
     path('faltas', FaultViewAPI.as_view(), name='faltas_api'),
     path('api-token', views.obtain_auth_token, name="tokenapi"),
-    path('professores', TeacherViewAPI.as_view(), name='professores' )
+    path('professores', TeacherViewAPI.as_view(), name='professores' ),
+    path('disciplinas', SubjectViewAPI.as_view())
 ]
