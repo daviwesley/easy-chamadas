dd70e86d15f57aded71e186e0efd7c043b36c47a
Davi Wesley <daviwesley@alu.ufc.br>
2018-10-08 15:03:32 -0300

    urls adicionada

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from .views import (StudentViewAPI, FaultViewAPI, StudentSearchViewAPI, TeacherViewAPI,\
-SubjectViewAPI)
+SubjectViewAPI, SubjectUpdateView, TeacherUpdateView, StudentUpdateView, FaultViewUpdate)
 from rest_framework.authtoken import views
 
 urlpatterns = [
+    # metodos update e put
+    path('alunos/update/<int:pk', StudentUpdateView.as_view()),
+    path('disciplinas/<int:pk', SubjectUpdateView.as_view()),
+    path('professores/<int:pk', TeacherUpdateView.as_view()),
+    path('faltas/<int:pk', FaultViewUpdate.as_view()),
     path('alunos', StudentViewAPI.as_view(), name='alunos'),
     path('alunos/<int:id>', StudentSearchViewAPI.as_view(), name='procura_aluno'),
     path('faltas', FaultViewAPI.as_view(), name='faltas_api'),
