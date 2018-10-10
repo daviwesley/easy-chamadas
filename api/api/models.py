407c4fdaebc344d84f63b01f1eed1d40e72c839c
Davi Wesley <daviwesley@alu.ufc.br>
2018-10-10 14:26:32 -0300

    refactored

--- api/api/models.py
+++ api/api/models.py
@@ -6,6 +6,7 @@ from rest_framework.authtoken.models import Token
 
 # Create your models here.
 
+
 class Teacher(models.Model):
     name = models.CharField(verbose_name='nome', max_length=55)
 
@@ -31,27 +32,24 @@ class Subject(models.Model):
 
 
 class Course(models.Model):
-    COURSES = (
-    ('ES', 'Engenharia de Software'),
-    ('CC', 'Ciências da Computação'),
-    ('EP', 'Engenharia da Produção'),
-    ('EC', 'Engenharia Civil'),
-    ('EM', 'Engenharia Mecânica'),
-    )
+    COURSES = (('ES', 'Engenharia de Software'),
+               ('CC', 'Ciências da Computação'),
+               ('EP', 'Engenharia da Produção'),
+               ('EC', 'Engenharia Civil'),
+               ('EM', 'Engenharia Mecânica'),)
     name = models.CharField(max_length=30, choices=COURSES)
 
 
 class Student(models.Model):
-    COURSES = (
-    ('ES', 'Engenharia de Software'),
-    ('CC', 'Ciências da Computação'),
-    ('EP', 'Engenharia da Produção'),
-    ('EC', 'Engenharia Civil'),
-    ('EM', 'Engenharia Mecânica'),
-    )
+    COURSES = (('ES', 'Engenharia de Software'),
+               ('CC', 'Ciências da Computação'),
+               ('EP', 'Engenharia da Produção'),
+               ('EC', 'Engenharia Civil'),
+               ('EM', 'Engenharia Mecânica'),)
 
     name = models.CharField(max_length=55, verbose_name='nome')
-    id_subscription = models.IntegerField(primary_key=True, verbose_name='matricula')
+    id_subscription = models.IntegerField(primary_key=True,
+                                          verbose_name='matricula')
     subject = models.ManyToManyField(Subject, related_name='rel_student')
     course = models.CharField(max_length=30, choices=COURSES)
 
@@ -61,27 +59,30 @@ class Student(models.Model):
     def __str__(self):
         return self.name
 
+
 class Situation(models.Model):
-    SITUACAO = (
-    ('REP', 'Reprovado por falta'),
-    ('ATV', 'Ativo'),
-    )
+    SITUACAO = (('REP', 'Reprovado por falta'),
+                ('ATV', 'Ativo'),)
     sit = models.CharField(max_length=30, choices=SITUACAO, default='ATV')
 
     class Meta:
         verbose_name = 'Situação'
 
+
 class Fault(models.Model):
     faults = models.IntegerField(verbose_name='Faltas')
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name='Aluno')
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name='Disciplina')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE,
+                                verbose_name='Aluno')
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE,
+                                verbose_name='Disciplina')
     day = models.DateField(auto_now=True, verbose_name="Data")
 
     class Meta:
         verbose_name = 'Falta'
-    
+
     def __str__(self):
         return self.student.name
+
 
 # cria um token quando um usuário é criado
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
