51e0ce65c32960ec7baa87f2c9e9d9688e7007a6
Davi Wesley <daviwesley@alu.ufc.br>
2018-10-10 14:27:53 -0300

    refactored

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,19 +1,24 @@
-from django.contrib import admin
 from django.urls import path
-from .views import (StudentViewAPI, FaultViewAPI, StudentSearchViewAPI, TeacherViewAPI,\
-SubjectViewAPI, SubjectUpdateView, TeacherUpdateView, StudentUpdateView, FaultViewUpdate)
+from .views import (StudentViewAPI, FaultViewAPI, StudentSearchViewAPI,
+                    TeacherViewAPI, SubjectViewAPI, SubjectUpdateView,
+                    TeacherUpdateView, StudentUpdateView, FaultViewUpdate)
 from rest_framework.authtoken import views
 
 urlpatterns = [
-    # metodos update e put
-    path('alunos/update/<int:pk', StudentUpdateView.as_view()),
-    path('disciplinas/<int:pk', SubjectUpdateView.as_view()),
-    path('professores/<int:pk', TeacherUpdateView.as_view()),
-    path('faltas/<int:pk', FaultViewUpdate.as_view()),
+    # Students
     path('alunos', StudentViewAPI.as_view(), name='alunos'),
-    path('alunos/<int:id>', StudentSearchViewAPI.as_view(), name='procura_aluno'),
+    path('alunos/update/<int:pk>', StudentUpdateView.as_view()),
+    path('alunos/<int:id>', StudentSearchViewAPI.as_view(),
+         name='procura_aluno'),
+    # Subjects
+    path('disciplinas', SubjectViewAPI.as_view()),
+    path('disciplinas/<int:pk>', SubjectUpdateView.as_view()),
+    # Teachers
+    path('professores', TeacherViewAPI.as_view(), name='professores'),
+    path('professores/<int:pk>', TeacherUpdateView.as_view()),
+    # Faults
     path('faltas', FaultViewAPI.as_view(), name='faltas_api'),
+    path('faltas/<int:pk>', FaultViewUpdate.as_view()),
+    # Authentication
     path('api-token', views.obtain_auth_token, name="tokenapi"),
-    path('professores', TeacherViewAPI.as_view(), name='professores' ),
-    path('disciplinas', SubjectViewAPI.as_view())
 ]
