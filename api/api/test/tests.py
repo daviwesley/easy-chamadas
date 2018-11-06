14fcb88e5513c430b0702a651d6b4e0609238244
daviwesley <daviwesley@alu.ufc.br>
2018-11-05 23:32:16 -0300

    refatoração e testes adicionados

--- api/api/test/tests.py
+++ api/api/test/tests.py
@@ -1,11 +1,11 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.contrib.auth.models import User
 
 from rest_framework.test import APIClient
-
 from model_mommy import mommy
+from rest_framework.authtoken.models import Token
 
-from api.models import Student, Subject, Teacher, Fault
+from api.models import Student, Subject, Teacher, Fault, Attendance
 
 
 class TestDatabase(TestCase):
@@ -68,25 +68,114 @@ class TestDatabase(TestCase):
         # Assert
         self.assertTrue(isinstance(user, User))
 
+    def test_create_attendance_log(self):
+        # Arrange
+        aluno = mommy.make(Student, name='Erwin Smith')
+        sub = mommy.make(Subject, name='Exploração')
+        att = mommy.make(Attendance, student=aluno, subject=sub)
+        # Act
+        # Assert
+        self.assertTrue(isinstance(att, Attendance))
+
+
 
 class TestURLsemToken(TestCase):
     # fixtures = ['datadumped']
 
     def setUP(self):
-        self.client = Client()
+        self.client = APIClient()
         # self.api_client = APIClient()
 
-    def test_url_api_faults_without_token(self):
+    # GET methods -----------------------
+    def test_url_api_faults_get(self):
+        # Assert
         response = self.client.get('/api/faltas')
+        # Act
+        # Assert
         self.assertEquals(response.status_code, 403)
 
-    def test_url_api_students(self):
+    def test_url_api_students_get(self):
+        # Assert
         response = self.client.get('/api/alunos')
+        # Act
+        # Assert
         self.assertEquals(response.status_code, 401)
 
-    def test_url_api_students_id(self):
+    def test_url_api_students_id_get(self):
+        # Assert
         response = self.client.get('/api/alunos/381097')
+        # Act
+        # Assert
         self.assertEquals(response.status_code, 401)
+
+    def test_url_api_attendance_get(self):
+        # Assert
+        response = self.client.get('/api/presencas')
+        # Act
+        # Assert
+        self.assertEquals(response.status_code, 401)
+
+    # POST methods ---------------------------
+    def test_url_api_faults_post(self):
+        # Arrange
+        url = '/api/faltas'
+        data = {
+            "faults": 2,
+            "student": {"name": "Eren Yager"},
+            "subject": {"name": "Estrutura de Dados"}
+        }
+        # Act
+        response = self.client.post(url, data, format='json')
+        # Assert
+        self.assertEquals(response.status_code, 403)
+
+    def test_url_api_subject_post(self):
+         # Arrange
+        url = '/api/disciplinas'
+        data = {
+            "name": "Teatro",
+            "teacher": {"name": "Rosalina Meireles"}
+        }
+        # Act
+        response = self.client.post(url, data, format='json')
+        # Assert
+        self.assertEquals(response.status_code, 403)
+
+    def test_url_api_student_post(self):
+        # Arrange
+        url = '/api/alunos'
+        data = {
+            "name": "Annie Leonheart",
+            "id_subscription": 432634,
+            "course": "Engenharia de Software"
+        }
+        # Act
+        response = self.client.post(url, data, format='json')
+        # Assert
+        self.assertEquals(response.status_code, 401)
+
+    def test_url_api_teacher_post(self):
+        # Arrange
+        data = {
+            'name':'Armin Armlet'
+        }
+        url = '/api/professores'
+        # Act
+        response = self.client.post(url, data, format='json')
+        # Assert
+        self.assertEqual(response.status_code, 401)
+
+    def test_url_attendance_post(self):
+        # Arrange
+        data = {
+            "student": "Daniel Wesley",
+            "subject": "Teatro"
+        }
+        url = '/api/presencas'
+        # Act
+        response = self.client.post(url, data, format='json')
+        # Assert
+        self.assertEqual(response.status_code, 401)
 
 
 class TestaURLcomToken(TestCase):
@@ -99,6 +188,8 @@ class TestaURLcomToken(TestCase):
                          password="ackerman948").auth_token)
         self.client.credentials(HTTP_AUTHORIZATION="Token {}".
                                 format(self.token))
+    def test_received_token(self):
+        self.assertEquals(self.token, Token.objects.get(user_id=1).__str__())
 
     def testa_url_professores_deve_retorna_200(self):
         response = self.client.get('/api/professores')
