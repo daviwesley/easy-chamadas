1d677a736655664a8bc2f4ec814399357afcfda1
daviwesley <daviwesley@alu.ufc.br>
2018-11-05 23:34:47 -0300

    tela de login reformulada

--- app/chamada-virtual/components/Login/LoginScreen.js
+++ app/chamada-virtual/components/Login/LoginScreen.js
@@ -1,102 +1,134 @@
 import React from 'react';
 import {
-  View, Text, TextInput, KeyboardAvoidingView, Button,
-  StyleSheet, AsyncStorage, Alert, ScrollView, Platform
+	View, Text, KeyboardAvoidingView,
+	StyleSheet, AsyncStorage, Alert, ScrollView, Platform
 } from 'react-native';
+import { Button, FormInput } from 'react-native-elements'
 
+//funções e variaveis
 import { fazerLogin } from '../../controllers'
 
 export class LoginScreen extends React.Component {
 	static navigationOptions = {
 		title: 'Login',
 	};
-  constructor(props) {
-    super(props)
-    this.state = {
-      usuario: "",
-      senha: ""
-    }
-  }
-  componentWillMount() {
-    AsyncStorage.getItem('token', (err, result) => {
-      if (result !== null) {
-        // Alert.alert("Ja temos seu login")
-        // Alert.alert(result)
-        this.props.navigation.push("Home")
-      } else {
-        Alert.alert("Seja Bem Vindo!", "Faça o login para prosseguir :)")
-      }
-    });
-  }
-  fazerLogin() {
-    //Alert.alert(this.state.usuario, this.state.senha)
-    fazerLogin(this.state.usuario, this.state.senha)
-      .then(result => {
-        AsyncStorage.setItem('token', JSON.stringify(result.token));
-        this.props.navigation.navigate("Home")
-      }).catch(erro => {
-        if (erro.non_field_errors) {
-          Alert.alert("Erro", "Usuário ou senha incorretas")
-        } else if (erro.username || erro.password) {
-          Alert.alert("Erro", "Preencha todos os campos")
-        } else {
-          Alert.alert("Erro", "Sem conexão com a internet")
-        }
-      })
-  }
-  render() {
-    return (
-      <KeyboardAvoidingView behavior="padding" enabled={Platform.OS === 'ios'} >
-        {/* <StatusBar backgroundColor='black'/> */}
-        <ScrollView>
-          <View style={{ alignItems: 'center', }}>
-            <Text style={styles.headerText}>Nome do usuário</Text>
-            <TextInput placeholder="Digite o nome da disciplina"
-              style={styles.textInput}
-			  autoCapitalize="none"
-			  autoCorrect={false}
-              value={this.props.usuario}
-              returnKeyType='next'
-              onSubmitEditing={() => { this.secondTextInput.focus(); }}
-              blurOnSubmit={false}
-              onChangeText={text => this.setState({ usuario: text })}
-            />
-            <Text style={styles.headerText}>Senha</Text>
-			<TextInput placeholder="Digite a senha do usuário"
-			  ref={el => this.secondTextInput = el}
-              style={styles.textInput}
-              autoCapitalize="none"
-              value={this.props.senha}
-              secureTextEntry={true}
-              password={true}
-              returnKeyType='send'
-              onSubmitEditing={() => {this.fazerLogin()}}
-              blurOnSubmit={false}
-			  onChangeText={text => this.setState({ senha: text })}
-            />
-          </View>
-          <Button title="Entrar" onPress={() => this.fazerLogin()}
-            accessibilityLabel="Entrar" />
-        </ScrollView>
-      </KeyboardAvoidingView>
-    );
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			usuario: "",
+			senha: ""
+		}
+	}
+	componentWillMount() {
+		AsyncStorage.getItem('token', (err, result) => {
+			if (result !== null) {
+				// Alert.alert("Ja temos seu login")
+				// Alert.alert(result)
+				this.props.navigation.push("Home")
+			} else {
+				Alert.alert("Seja Bem Vindo!", "Faça o login para prosseguir :)")
+			}
+		});
+	}
+	fazerLogin() {
+		//Alert.alert(this.state.usuario, this.state.senha)
+		fazerLogin(this.state.usuario, this.state.senha)
+			.then(result => {
+				AsyncStorage.setItem('token', JSON.stringify(result.token));
+				this.props.navigation.navigate("Home")
+			}).catch(erro => {
+				if (erro.non_field_errors) {
+					Alert.alert("Erro", "Usuário ou senha incorretas")
+				} else if (erro.username || erro.password) {
+					Alert.alert("Erro", "Preencha todos os campos")
+				} else {
+					Alert.alert("Erro", "Sem conexão com a internet")
+				}
+			})
+	}
+	render() {
+		return (
+			<ScrollView contentContainerStyle={styles.container}>
+				<KeyboardAvoidingView behavior="padding" enabled={Platform.OS === 'ios'} >
+					{/* <StatusBar backgroundColor='black'/> */}
+					<View>
+						<Text style={styles.headerText}>Nome do usuário</Text>
+						<FormInput
+							placeholder='Digite seu usuário'
+							returnKeyType='next'
+							underlineColorAndroid='transparent'
+							blurOnSubmit={false}
+							containerStyle={styles.formInputContainer}
+							inputStyle={styles.textInput}
+							clearButtonMode='always'
+							autoCapitalize='none'
+							autoCorrect={false}
+							onChangeText={text => this.setState({ usuario: text })}
+							onSubmitEditing={() => {this.secondTextInput.focus()}}/>
+						<Text style={styles.headerText}>Senha</Text>
+						<FormInput
+							secureTextEntry={true}
+							placeholder='Digite sua senha'
+							ref={el => this.secondTextInput = el}
+							returnKeyType='go'
+							underlineColorAndroid='transparent'
+							blurOnSubmit={false}
+							containerStyle={styles.formInputContainer}
+							inputStyle={styles.textInput}
+							clearButtonMode='always'
+							autoCapitalize='none'
+							autoCorrect={false}
+							onChangeText={text => this.setState({ senha: text })}
+							onSubmitEditing={() => { this.fazerLogin() }}/>
+					</View>
+					<Button title="Entrar" onPress={() => this.fazerLogin()}
+						accessibilityLabel="Entrar"
+						buttonStyle={styles.buttonStyle}
+						fontSize={15}
+						containerViewStyle={styles.buttonContainer}
+						/>
+				</KeyboardAvoidingView>
+			</ScrollView>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  textInput: {
-    height: 40,
-    width: "95%",
-    borderColor: 'black',
-    borderRadius: 4,
-    borderWidth: 1,
-    marginBottom: 20,
-    backgroundColor: 'white',
-  },
-  headerText: {
-    fontSize: 15,
-    paddingTop: 5,
-  }
+	textInput: {
+		color:'#000000',
+		height: 43,
+		fontSize:15,
+		width: "100%",
+		borderColor: 'black',
+		borderRadius: 4,
+		borderWidth: 1,
+		marginBottom: 1,
+		backgroundColor: 'white',
+	},
+	headerText: {
+		marginLeft:2,
+		fontSize: 15,
+		paddingTop: 5,
+	},
+	container:{
+		flex:1,
+		justifyContent:'center'
+	},
+	formInputContainer: {
+		marginLeft:2,
+		marginRight:2,
+		borderBottomColor: 'black',
+  	borderBottomWidth: 0,
+	},
+	buttonContainer:{
+		marginLeft:2,
+		marginRight:2,
+		paddingTop:5
+	},
+	buttonStyle:{
+		backgroundColor:'black',
+		borderRadius:4,
+	}
 });
 
 
