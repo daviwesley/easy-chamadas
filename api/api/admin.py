d55cb9687e75b776268b8b8d62015e5e9b22b8bf
daviwesley <daviwesley@alu.ufc.br>
2018-11-05 23:27:27 -0300

    adiciona tabela de presenca para o painel administrativo

--- api/api/admin.py
+++ api/api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Teacher, Student, Subject, Fault
+from .models import Teacher, Student, Subject, Fault, Attendance
 # Register your models here.
 
 admin.site.register(Teacher)
 admin.site.register(Student)
 admin.site.register(Subject)
 admin.site.register(Fault)
+admin.site.register(Attendance)
